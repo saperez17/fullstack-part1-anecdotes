be955c3ec3f067c7221a7fe3d785e4d4cbee9a55
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-06-09 09:36:31 -0500

    Part1. Anecdote. Final version Includes anecdote of the day, voting button, and anecdote with the most votes.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 const App = () => {
@@ -11,8 +12,42 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients"
   ];
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(7).fill(0));
 
-  return <div>{anecdotes[selected]}</div>;
+  const voteHandler = () => {
+    let pointsCopy = [...points];
+    pointsCopy[selected] += 1;
+    setPoints(pointsCopy);
+  };
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <p>
+        {anecdotes[selected]} has {points[selected]} points.
+      </p>
+      <div>
+        <button type="button" onClick={voteHandler}>
+          vote
+        </button>
+        <button
+          type="button"
+          onClick={() => setSelected(Math.floor(Math.random() * 7))} //arrow function
+          //returns a random integer from 0 to 7
+        >
+          next anecdote
+        </button>
+      </div>
+
+      <div>
+        <h2>Anecdote with the most votes</h2>
+        <p>
+          {anecdotes[points.indexOf(Math.max(...points))]} has{" "}
+          {Math.max(...points)} points.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
